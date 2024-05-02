4b27e0e469e8eed28c189237d15c5ad2d9a1645c
Jobayer Mojumder <jobayer.mojumder@burda-forward.de>
2024-05-02 20:58:13 +0200

    initial commit

---
+++ src/Calculator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace JobayerMojumder\Calculator;
+
+class Calculator
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b)
+    {
+        return $a - $b;
+    }
+
+    public function multiply($a, $b)
+    {
+        return $a * $b;
+    }
+
+    public function division($a, $b)
+    {
+        return $a / $b;
+    }
+}
